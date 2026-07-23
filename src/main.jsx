1e01c22b15f09c9ef530f3513b13f51fa4f177a0
tikowikoFamily <mrgamerdu84@gmail.com>
2026-07-23 16:20:39 +0200

    Activer les réseaux de transport selon la ville

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 import "./personnalisation.js";
-import "./transports.js";
+import "./transports-dynamiques.js";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
